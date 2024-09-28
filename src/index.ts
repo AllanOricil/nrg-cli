bfc9e70cbbcb58e89804aecffe7b377841de7701
Allan Oricil <allanoricilcos@outlook.com>
2024-09-27 22:14:22 -0300

    feat: add first iteration of create command

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,16 @@ import {
 import * as path from "path";
 import * as fs from "fs";
 import { fileURLToPath } from "url";
+import nodePlop from "node-plop";
+import chalk from "chalk";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const plopfilePath = path.resolve(
+  __dirname,
+  "../node_modules/@allanoricil/nrg-generator/plopfile.js",
+);
 
 interface PackageJson {
   name: string;
@@ -104,6 +111,132 @@ yargs(hideBin(process.argv))
 
       await build(config);
       await startNodeRed(config, listener);
+    },
+  )
+  .command<{
+    type: "" | "node";
+    projectName: "string";
+    nodeName: "string";
+    nodeCategory: "string";
+    nodeColor: "string";
+    nodeInputs: "number";
+    nodeOutputs: "number";
+  }>(
+    "create [type]",
+    "Create a new nrg project or node.",
+    (yargs) => {
+      yargs
+        .positional("type", {
+          describe:
+            "Specify the type of item to create. If left empty, a project will be created.",
+          choices: ["node"],
+        })
+        .option("project-name", {
+          type: "string",
+          description: "What would you like to name your project?",
+        })
+        .option("node-name", {
+          type: "string",
+          description: "What would you like to name your node?",
+        })
+        .option("node-category", {
+          type: "string",
+          description: "Which category should your node belong to?",
+        })
+        .option("node-color", {
+          type: "string",
+          description: "What color should represent your node? (e.g. #FFFFFF)",
+        })
+        .option("node-inputs", {
+          type: "number",
+          description: "How many inputs should your node have? (e.g. 1)",
+        })
+        .option("node-outputs", {
+          type: "number",
+          description: "How many outputs should your node have? (e.g. 1)",
+        })
+        .check((argv) => {
+          const type = argv.type;
+          if (type === "node" && !argv["node-name"]) {
+            throw new Error(
+              'The --node-name option is required when type is "node".',
+            );
+          }
+          if (type !== "node" && !argv["project-name"]) {
+            throw new Error(
+              "The --project-name option is required when type is undefined",
+            );
+          }
+          return true;
+        })
+        .example(
+          "nrg create --project-name nrg-project",
+          "Creates a new project.",
+        )
+        .example(
+          "nrg create node --node-name node-1",
+          "Creates a new node named node-1.",
+        );
+    },
+    async (argv) => {
+      const {
+        type,
+        projectName,
+        nodeName,
+        nodeCategory,
+        nodeColor,
+        nodeInputs,
+        nodeOutputs,
+      } = argv;
+
+      const plop = await nodePlop(plopfilePath);
+      if (!type) {
+        const generator = plop.getGenerator("create");
+        const cliAnswers = [
+          projectName || "_",
+          nodeName || "_",
+          nodeCategory || "_",
+          nodeColor || "_",
+          nodeInputs || "_",
+          nodeOutputs || "_",
+        ];
+        const promptAnswers = await generator.runPrompts(cliAnswers);
+        await generator.runActions(promptAnswers);
+
+        console.log(
+          chalk.green(`\n🎉 Your project has been created successfully! 🎉`),
+        );
+        console.log(
+          chalk.green(`\nLet’s get started with a couple of quick steps:`),
+        );
+        console.log(chalk.green(`1. Navigate into your project directory:`));
+        console.log(
+          chalk.green(`   cd ${projectName || promptAnswers.projectName}`),
+        );
+        console.log(
+          chalk.green(
+            `2. Install the dependencies using your favorite package manager.`,
+          ),
+        );
+        console.log(
+          chalk.green(`\nHappy coding! If you need help, just ask! 🚀\n`),
+        );
+      } else if (type === "node") {
+        const generator = plop.getGenerator("create:node");
+        const cliAnswers = [
+          nodeName || "_",
+          nodeCategory || "_",
+          nodeColor || "_",
+          nodeInputs || "_",
+          nodeOutputs || "_",
+        ];
+        const promptAnswers = await generator.runPrompts(cliAnswers);
+        await generator.runActions(promptAnswers);
+
+        console.log(
+          chalk.green(`\n🎉 Your node has been created successfully! 🎉`),
+        );
+      }
     },
   )
   .demandCommand(1, "You need at least one command before moving on")
