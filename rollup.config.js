8782e51f3bf10992f4cfe8c212ca835ac77c06c3
Allan Oricil <allanoricilcos@outlook.com>
2024-10-02 11:53:19 -0300

    feat: configure sentry for telemetry data

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
+import replace from "@rollup/plugin-replace";
 
 export default [
   {
     input: "src/index.ts",
-    output: [{ file: "dist/index.js", format: "es" }],
-    plugins: [typescript()],
+    output: [{ file: "dist/index.js", format: "es", sourcemap: true }],
+    plugins: [
+      typescript(),
+      replace({
+        preventAssignment: true,
+        "process.env.RELEASE_TAG": JSON.stringify(process.env.RELEASE_TAG),
+      }),
+    ],
+    external: ["./node_modules/node-machine-id"],
   },
   {
     input: "src/index.ts",
---
+++ src/telemetry.ts
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+// @ts-expect-error package doesn't have proper types
+import machineId from "node-machine-uid";
+
+const userId = await machineId();
+Sentry.init({
+  dsn: "https://aa7944d0fe4c4b439140a0f6b7141d25@o4508053113470976.ingest.us.sentry.io/4508053115830272",
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+  enabled: !process.env.NRG_DISABLE_TELEMETRY,
+  release: process.env.RELEASE_VERSION,
+});
+
+Sentry.setUser({
+  id: userId,
+});
