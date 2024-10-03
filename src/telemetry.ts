edba54c88a91cc0198afb835fee2d69634559224
Allan Oricil <allanoricilcos@outlook.com>
2024-10-03 18:46:52 -0300

    fix: change node-machine-uid to machine-uuid
    node-machine-uid has a dependency on ansi-regex@2.1.1, which has a vulnerability
    
    ansi-regex Regular Expression Denial of Service (ReDoS)
    
    Introduced through
    node-machine-uid@1.0.2
    Fixed in
    ansi-regex@3.0.1, @4.1.1, @5.0.1, @6.0.1
    
    https://cwe.mitre.org/data/definitions/400.html
    https://www.cve.org/CVERecord?id=CVE-2021-3807
    https://www.first.org/cvss/calculator/3.1#CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:H/E:P/RL:O/RC:C

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -2,9 +2,9 @@ import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 // @ts-expect-error package doesn't have proper types
-import machineId from "node-machine-uid";
+import machineUuid from "machine-uuid";
 
-const userId = await machineId();
+const userId = await machineUuid();
 Sentry.init({
   dsn: "https://aa7944d0fe4c4b439140a0f6b7141d25@o4508053113470976.ingest.us.sentry.io/4508053115830272",
   integrations: [nodeProfilingIntegration()],
